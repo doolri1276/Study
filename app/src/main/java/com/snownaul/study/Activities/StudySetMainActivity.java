5814b2636973cbcd2e3f5f0dfbb76cd7b5324c56
snownaul2 <snownaul2@gmail.com>
2018-05-08 14:09:21 +0900

    commit

--- app/src/main/java/com/snownaul/study/Activities/StudySetMainActivity.java
+++ app/src/main/java/com/snownaul/study/Activities/StudySetMainActivity.java
@@ -2,6 +2,9 @@ package com.snownaul.study.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 
 import com.github.bluzwong.swipeback.SwipeBackActivityHelper;
@@ -11,6 +14,11 @@ public class StudySetMainActivity extends AppCompatActivity {
 
     SwipeBackActivityHelper helper=new SwipeBackActivityHelper();
 
+    TextView tvTitle,tvInfo;
+    ToggleButton tbFavor;
+    TextView tvFavorCnt, tvStudiedTotalCnt;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +26,32 @@ public class StudySetMainActivity extends AppCompatActivity {
 
         helper.setEdgeMode(true).setParallaxMode(true).setParallaxRatio(3).setNeedBackgroundShadow(true).init(this);
 
+        tvTitle=findViewById(R.id.tv_title);
+        tvInfo=findViewById(R.id.tv_info);
+        tbFavor=findViewById(R.id.tb_favor);
+        tvFavorCnt=findViewById(R.id.tv_favor_cnt);
+        tvStudiedTotalCnt=findViewById(R.id.tv_studied_totalcnt);
 
     }
 
     @Override
     public void onBackPressed() {
         helper.finish();
+    }
+
+    public void clickStorage(View v){
+
+    }
+
+    public void clickStudy(View v){
+
+    }
+
+    public void clickPlay(View v){
+
+    }
+
+    public void clickReport(View v){
+
     }
 }
