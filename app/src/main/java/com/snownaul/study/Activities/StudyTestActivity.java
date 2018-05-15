b807cb27ff1050fc28d752570384c48b23d0a1d3
snownaul2 <snownaul2@gmail.com>
2018-05-15 14:31:24 +0900

    commit

--- app/src/main/java/com/snownaul/study/Activities/StudyTestActivity.java
+++ app/src/main/java/com/snownaul/study/Activities/StudyTestActivity.java
@@ -2,14 +2,54 @@ package com.snownaul.study.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.snownaul.study.R;
 
 public class StudyTestActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_test);
+
+        toolbar=findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.studytest_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+
+            case R.id.setting:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clickBtn(View v){
+
     }
 }
